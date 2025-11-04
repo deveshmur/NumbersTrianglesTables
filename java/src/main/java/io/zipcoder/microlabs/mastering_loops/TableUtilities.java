fc9dca9ae702a84a2e8d69fcc44449501dfcab22
deveshmur <dmdevesh@gmail.com>
2025-11-04 11:26:28 -0500

    completed TableUtilities and tested

--- java/src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ java/src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,27 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder table = new StringBuilder();
+
+        for (int row = 1; row <= tableSize; row++) {
+            for (int col = 1; col <= tableSize; col++) {
+                int product = row * col;
+                
+                table.append(String.format("%3d |", product));
+            }
+            table.append("\n");
+        }
+
+        return table.toString();
     }
 
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5); 
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10); 
     }
 }
