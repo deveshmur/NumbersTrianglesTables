20d452831997acc538f458214431d31aaa3d6986
deveshmur <dmdevesh@gmail.com>
2025-11-04 11:14:58 -0500

    completed NumberUtilities and tested

--- java/src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ java/src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,41 +1,66 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-
 public class NumberUtilities {
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += (int) Math.pow(i, exponent);
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
     public static String getRange(int stop) {
-        return null;
+        return getRange(0, stop, 1);
     }
 
-    /*
-     * natural break
-     */
+  
 
-    public static boolean isNumberEven(int toTest) { return false; }
-    public static boolean isNumberOdd(int toTest) { return false; }
+    public static boolean isNumberEven(int toTest) { 
+        return toTest % 2 == 0; 
+    }
+
+    public static boolean isNumberOdd(int toTest) { 
+        return toTest % 2 != 0; 
+    }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberEven(i)) {
+                result += i;
+            }
+        }
+        return result;
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberOdd(i)) {
+                result += i;
+            }
+        }
+        return result;
     }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += (i * i);
+        }
+        return result;
     }
-
 }
